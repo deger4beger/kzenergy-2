b3d589a76d10267fa03fea2394ce9d7ccb04b390
deger4beger <deger.begerrr@gmail.com>
2022-05-31 20:44:47 +0600

    some fixes

--- src/app/validators/token.ts
+++ src/app/validators/token.ts
@@ -2,14 +2,19 @@ import decode, { JwtPayload } from "jwt-decode"
 import { UserData } from "../../types/user"
 
 export const isTokenValid = () => {
-	const user = localStorage.getItem("user")
-	if (!user) {
+	try {
+		const user = localStorage.getItem("user")
+		if (!user) {
+			return false
+		}
+		const userData: UserData = JSON.parse(user)
+	  const decodedToken = decode<JwtPayload>(userData.token)
+		if (decodedToken.exp && (decodedToken.exp * 1000 > new Date().getTime())) {
+			return userData
+		}
+		localStorage.removeItem("user")
 		return false
+	} catch (e) {
+		localStorage.removeItem("user")
 	}
-	const userData: UserData = JSON.parse(user)
-  const decodedToken = decode<JwtPayload>(userData.token)
-	if (decodedToken.exp && (decodedToken.exp * 1000 > new Date().getTime())) {
-		return userData
-	}
-	return false
 }
