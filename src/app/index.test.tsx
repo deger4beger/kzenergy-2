378c97dd59e3357ca96beb3daf2a3b0f9237eded
deger4beger <deger.begerrr@gmail.com>
2022-06-04 13:56:49 +0600

    app comp. test added

---
+++ src/app/index.test.tsx
@@ -0,0 +1,56 @@
+import Header from "./parts/Header"
+import Footer from "./parts/Footer"
+import AppRouter from "./components/AppRouter"
+import Preloader from "./components/Preloader"
+import { render, screen } from "../lib/utils/test/redux"
+import * as ReduxHooks from "./hooks/redux"
+import App from "."
+
+jest.mock("./parts/Header", () => ({
+    __esModule: true,
+    default: jest.fn(() => (
+        <div data-testid="Header" />
+    ))
+}))
+jest.mock("./parts/Footer", () => ({
+    __esModule: true,
+    default: jest.fn(() => (
+        <div data-testid="Footer" />
+    ))
+}))
+jest.mock("./components/AppRouter", () => ({
+    __esModule: true,
+    default: jest.fn(() => (
+        <div data-testid="AppRouter" />
+    ))
+}))
+jest.mock("./components/Preloader", () => ({
+    __esModule: true,
+    default: jest.fn(() => (
+        <div data-testid="Preloader" />
+    ))
+}))
+
+describe("App component", () => {
+	it("Preloader is shown before other components", () => {
+
+		jest.spyOn(ReduxHooks, "useAppSelector").mockReturnValueOnce(false)
+
+		render(<App />)
+
+		expect(screen.queryByTestId("Preloader")).toBeInTheDocument()
+		expect(screen.queryByTestId("AppRouter")).not.toBeInTheDocument()
+
+	})
+	it("All components are shown after initialization", async () => {
+
+		render(<App />)
+
+		expect(await screen.findByTestId("Header")).toBeInTheDocument()
+		expect(await screen.findByTestId("Footer")).toBeInTheDocument()
+		expect(await screen.findByTestId("AppRouter")).toBeInTheDocument()
+
+		expect(screen.queryByTestId("Preloader")).not.toBeInTheDocument()
+
+	})
+})
