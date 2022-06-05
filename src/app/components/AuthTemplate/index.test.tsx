2577d4e61e80b4eb617c4d230e7a6a26d0abc803
deger4beger <deger.begerrr@gmail.com>
2022-06-05 19:52:05 +0600

    AuthTemplate tests added

---
+++ src/app/components/AuthTemplate/index.test.tsx
@@ -0,0 +1,107 @@
+import { Router } from "react-router-dom"
+import { createMemoryHistory } from "history"
+import { screen } from "../../../lib/utils/test/redux"
+import { render } from "@testing-library/react"
+import AuthTemplate from "."
+import userEvent from "@testing-library/user-event"
+
+describe("AuthTemplate component", () => {
+
+	it("Rendered with props", () => {
+
+		const history = createMemoryHistory()
+		history.push = jest.fn()
+
+		render(
+		  <Router history={history as any}>
+		    <AuthTemplate
+		      title="title"
+		      button={<div data-testid="button" />}
+		      isSigninForm={false}
+		    >
+		      <div data-testid="children" />
+		    </AuthTemplate>
+		  </Router>
+		)
+
+		expect(screen.queryByText(/title/i)).toBeInTheDocument()
+		expect(screen.queryByTestId(/button/i)).toBeInTheDocument()
+		expect(screen.queryByTestId(/children/i)).toBeInTheDocument()
+
+	})
+
+	it("Error is shown", () => {
+
+		const history = createMemoryHistory()
+		history.push = jest.fn()
+
+		render(
+		  <Router history={history as any}>
+		    <AuthTemplate
+		      title="title"
+		      button={<div />}
+		      isSigninForm={false}
+		      error={"some error"}
+		    >
+		      <div />
+		    </AuthTemplate>
+		  </Router>
+		)
+
+		expect(screen.queryByText(/some error/i)).toBeInTheDocument()
+
+	})
+
+	it("First link is shown and works well", () => {
+
+		const history = createMemoryHistory()
+		history.push = jest.fn()
+
+		render(
+		  <Router history={history as any}>
+		    <AuthTemplate
+		      title="title"
+		      button={<div />}
+		      isSigninForm={true}
+		      error={"some error"}
+		    >
+		      <div />
+		    </AuthTemplate>
+		  </Router>
+		)
+
+		expect(screen.queryByText(/signup/i)).toBeInTheDocument()
+		userEvent.click(screen.getByText(/signup/i))
+		expect(history.push).toHaveBeenCalledWith("/signup")
+
+		expect(screen.queryByText(/login/)).not.toBeInTheDocument()
+
+	})
+
+	it("Seconds link is shown and works well", () => {
+
+		const history = createMemoryHistory()
+		history.push = jest.fn()
+
+		render(
+		  <Router history={history as any}>
+		    <AuthTemplate
+		      title="title"
+		      button={<div />}
+		      isSigninForm={false}
+		      error={"some error"}
+		    >
+		      <div />
+		    </AuthTemplate>
+		  </Router>
+		)
+
+		expect(screen.queryByText(/login/i)).toBeInTheDocument()
+		userEvent.click(screen.getByText(/login/i))
+		expect(history.push).toHaveBeenCalledWith("/signin")
+
+		expect(screen.queryByText(/signup/)).not.toBeInTheDocument()
+
+	})
+
+})
