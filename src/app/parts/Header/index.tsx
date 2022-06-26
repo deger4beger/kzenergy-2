eebcf81e4d2bbbf6df016dc925a5e542b5c00ed6
deger4beger <deger.begerrr@gmail.com>
2022-06-26 23:25:00 +0600

    @latest

--- src/app/parts/Header/index.tsx
+++ src/app/parts/Header/index.tsx
@@ -7,10 +7,6 @@ const Header = () => {
 			<div className={s.container}>
 				<div className={s.logo}>
 					shoqan-inc.
-					{/*<span className={s.logoIcon}>⛭</span>*/}
-					{/*<img src={"assets/icons/logo.png"} alt="#" className={s.logoIcon} draggable={false} />*/}
-					{/*<div className={s.tickX} />
-					<div className={s.tickY} />*/}
 				</div>
 			</div>
 		</div>
