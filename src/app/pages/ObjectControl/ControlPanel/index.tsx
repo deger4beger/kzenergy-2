3c038c3e1c7e6524feae41a39e26ec7a5b062a18
deger4beger <deger.begerrr@gmail.com>
2022-07-01 21:44:49 +0600

    Modal added

--- src/app/pages/ObjectControl/ControlPanel/index.tsx
+++ src/app/pages/ObjectControl/ControlPanel/index.tsx
@@ -1,14 +1,25 @@
+import Modal from "app/components/Modal"
+import { useState } from "react"
 import s from "./index.module.scss"
 
 const ControlPanel = () => {
+
+	const [modalActive, setModalActive] = useState(false)
+
 	return (
 		<div className={s.wrapper}>
 			<div className={s.title}>
 				List of your objects
 			</div>
-			<div className={s.addBtn}>
+			<div className={s.addBtn} onClick={() => setModalActive(true)}>
 				+
 			</div>
+			<Modal
+				active={modalActive}
+				setActive={setModalActive}
+			>
+				Create object
+			</Modal>
 		</div>
 	)
 }
