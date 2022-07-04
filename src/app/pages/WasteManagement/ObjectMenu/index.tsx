89d8c2a303bab4a36e26b4060f8192eae429bf43
deger4beger <deger.begerrr@gmail.com>
2022-07-04 22:54:21 +0600

    @latest

--- src/app/pages/WasteManagement/ObjectMenu/index.tsx
+++ src/app/pages/WasteManagement/ObjectMenu/index.tsx
@@ -1,33 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import GroupLayout from "app/components/GroupLayout"
 import SimpleButton from "app/components/SimpleButton"
 import { ObjectData } from "types/object"
+import SelectDocumentType from "./SelectDocumentType"
 
 
 interface Props {
 	object: ObjectData
 	onComeBackClick: () => void
 }
+export type DocumentType = null | "ticket" | "report"
 
 const ObjectMenu: React.FC<Props> = ({
 	object: { id, name, wastes },
 	onComeBackClick
 }) => {
 
-	return <>
+	const [documentType, setDocumentType] = useState<DocumentType>(null)
+
+	return (
 		<GroupLayout
 			title={name}
 			btns={ <SimpleButton
 				onClick={onComeBackClick}
 				text="← Вернуться к выбору объекта"
 			/> }>
-			<div style={{display: "flex", flexWrap: "wrap"}}>
-				Контент
+			<div style={{ display: "flex" }}>
 				{/*{ loading && <Preloader /> }*/}
+				<SelectDocumentType
+					setDocumentType={setDocumentType}
+					documentType={documentType}
+				/>
 			</div>
 		</GroupLayout>
-	</>
-
+	)
 }
 
 export default ObjectMenu
