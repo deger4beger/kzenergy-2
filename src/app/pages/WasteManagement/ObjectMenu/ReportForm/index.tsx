ca3c540d35a2c1b8148a416e4882dc816a230abb
deger4beger <deger.begerrr@gmail.com>
2022-07-09 15:47:45 +0600

    Refactor to component

---
+++ src/app/pages/WasteManagement/ObjectMenu/ReportForm/index.tsx
@@ -0,0 +1,77 @@
+import Button from "app/components/Button"
+import Modal from "app/components/Modal"
+import TalonCardMini from "app/components/TalonCardMini"
+import React from "react"
+import { Talon, TalonStatus } from "types/talon"
+
+interface Props {
+	active: boolean
+	setActive: (active: boolean) => void
+	tickets: Talon[]
+	createReport: () => void
+}
+
+const ReportForm: React.FC<Props> = ({
+	active,
+	setActive,
+	tickets,
+	createReport
+}) => {
+
+	const titleStyles = {
+		fontWeight: "500",
+		marginBottom: "4px",
+		textDecoration: "underline"
+	}
+
+	return (
+		<Modal
+			active={active}
+			setActive={setActive}
+			title="Создание отчета"
+		>
+			<div style={{
+				...titleStyles,
+			}}>
+				Создать отчет из талонов:
+			</div>
+			{ tickets
+					.filter(talon => talon.status === TalonStatus.ACCEPTED)
+					.map(talon =>
+						<TalonCardMini
+							key={talon.id}
+							talon={talon}
+						/>
+			) }
+			{ tickets.some(talon => talon.status !== TalonStatus.ACCEPTED) && <div>
+				<div style={{
+					...titleStyles,
+					marginTop: "10px"
+				}}>
+					Следующие талоны не будут использованы в отчете:
+				</div>
+				{ tickets
+						.filter(talon => talon.status !== TalonStatus.ACCEPTED)
+						.map(talon =>
+							<TalonCardMini
+								key={talon.id}
+								talon={talon}
+							/>
+				) }
+			</div> }
+			<Button
+				content="Создать отчет"
+				onClick={createReport}
+				disabled={false}
+				loading={false}
+				styles={{
+					borderRadius: "20px",
+					marginTop: "30px"
+				}}
+			/>
+		</Modal>
+	)
+
+}
+
+export default ReportForm
--- src/app/pages/WasteManagement/ObjectMenu/Reports/index.tsx
+++ src/app/pages/WasteManagement/ObjectMenu/Reports/index.tsx
@@ -1,12 +1,9 @@
-import Button from "app/components/Button"
 import GroupLayout from "app/components/GroupLayout"
-import Modal from "app/components/Modal"
 import SimpleButton from "app/components/SimpleButton"
-import TalonCardMini from "app/components/TalonCardMini"
 import { useState } from "react"
 import { Report } from "types/report"
-import { Talon, TalonStatus } from "types/talon"
-import s from "./index.module.scss"
+import { Talon } from "types/talon"
+import ReportForm from "../ReportForm"
 
 interface Props {
 	report: Report | null
@@ -19,15 +16,9 @@ const Reports: React.FC<Props> = ({
 }) => {
 
 	const [createReportActive, setCreateReportActive] = useState(false)
-
+	console.log(report)
 	const createReport = () => {
 		console.log("1")
-	}
-
-	const titleStyles = {
-		fontWeight: "500",
-		marginBottom: "4px",
-		textDecoration: "underline"
 	}
 
 	return (
@@ -40,54 +31,12 @@ const Reports: React.FC<Props> = ({
 			subLayout
 		>
 			<>
-			<div style={{ display: "flex" }}>
-
-			</div>
-			<Modal
-				active={createReportActive}
-				setActive={setCreateReportActive}
-				title="Создание отчета"
-			>
-				<div style={{
-					...titleStyles,
-				}}>
-					Создать отчет из талонов:
-				</div>
-				{ tickets
-						.filter(talon => talon.status === TalonStatus.ACCEPTED)
-						.map(talon =>
-							<TalonCardMini
-								key={talon.id}
-								talon={talon}
-							/>
-				) }
-				{ tickets.some(talon => talon.status !== TalonStatus.ACCEPTED) && <div>
-					<div style={{
-						...titleStyles,
-						marginTop: "10px"
-					}}>
-						Следующие талоны не будут использованы в отчете:
-					</div>
-					{ tickets
-							.filter(talon => talon.status !== TalonStatus.ACCEPTED)
-							.map(talon =>
-								<TalonCardMini
-									key={talon.id}
-									talon={talon}
-								/>
-					) }
-				</div> }
-				<Button
-					content="Создать отчет"
-					onClick={createReport}
-					disabled={false}
-					loading={false}
-					styles={{
-						borderRadius: "20px",
-						marginTop: "30px"
-					}}
+				<ReportForm
+					active={createReportActive}
+					setActive={setCreateReportActive}
+					createReport={createReport}
+					tickets={tickets}
 				/>
-			</Modal>
 			</>
 		</GroupLayout>
 	)
