ef0b055087b688207937ddcdb9e66e91e9420edd
deger4beger <deger.begerrr@gmail.com>
2022-07-25 18:07:11 +0600

    Done

--- src/app/components/Restrict/index.tsx
+++ src/app/components/Restrict/index.tsx
@@ -1,7 +1,28 @@
+import { logout } from "lib/redux/auth/slice"
+import { useDispatch } from "react-redux"
+import Button from "../Button"
 import s from "./index.module.scss"
 
-const Restrict = () => <div className={s.wrapper}>
-	По решению администратора, на данном аккаунте заблокирована функция просмотра информации
-</div>
+const Restrict = () => {
+
+	const dispatch = useDispatch()
+
+	const onLogout = () => {
+		dispatch(logout())
+	}
+
+	return (
+		<div className={s.wrapper}>
+			По решению администратора, на данном аккаунте заблокирована функция просмотра данных
+			<div className={s.btn}>
+				<Button
+					content="Выйти"
+					onClick={onLogout}
+				/>
+			</div>
+		</div>
+	)
+
+}
 
 export default Restrict
