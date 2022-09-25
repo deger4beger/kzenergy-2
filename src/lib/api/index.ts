1a1ad066dec3eeb0c329a3710c003a8fa7b4edf5
deger4beger <deger.begerrr@gmail.com>
2022-09-25 15:52:54 +0600

    api url env added

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,7 @@
 import Axios from "axios"
 import { axiosBaseQuery } from "./index.axiosquery"
 
-export const baseURL = "http://localhost:8000"
+export const baseURL = process.env.API_URL || "http://localhost:8000"
 
 // Full protected
 export const mainInstance = Axios.create({ baseURL })
